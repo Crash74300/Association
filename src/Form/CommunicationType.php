e0baa4405de1ac2683abab63fdc60148c7e16462
Alex <amalesieux@gmail.com>
2023-11-02 09:36:45 +0100

    Modification partie communication

--- src/Form/CommunicationType.php
+++ src/Form/CommunicationType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Communication;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class CommunicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description',TextType::class,[
+            ->add('description',TextareaType::class,[
                 'label'=> 'Description de votre projet de communication'
             ])
             ->add('imageFile1', VichImageType::class,[
